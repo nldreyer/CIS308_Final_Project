f811b0883edb9999373268a5eb6e574edb0d949f
Nicholas Dreyer <nldreyer@.ksu.edu>
2018-12-03 19:50:34 -0600

    Sync

--- main.c
+++ main.c
@@ -30,7 +30,6 @@ int main(void) {
     refresh();
     FILE *fps = fopen("Names.txt", "r");
     FILE *fpt = fopen("Teachers.txt", "r");
-    FILE *fpc = fopen("classList.txt", "r");
     char id_string[6];
     int signed_in = 0;
     int isTeacher = 0;
@@ -61,28 +60,52 @@ int main(void) {
 	    mvaddstr(14, 33, "User does not exist please enter new ID.");
 	    mvaddstr(13, 33, "Enter School ID: ");
             refresh();
-	    fclose(fps);
-	    fclose(fpt);
-	    fps = fopen("Names.txt", "r");
-	    fpt = fopen("Teachers.txt", "r");
+	    rewind(fps);
+	    rewind(fpt);
 	}
     }
     } while(signed_in == 0);
 	
     if(isTeacher == 1){
+	FILE *fpc = fopen(strcat(id_string, ".txt"), "r");
 	Teacher * teacher = malloc(sizeof(Teacher));
-	char buffer[50];
-	TempList * tempList = malloc(sizeof(TempList));
-	TempList * list = tempList;
-	while(fscanf(fpc, "%s", buffer) != EOF){
+	long lSize;
+	char * buffer;
+
+	fseek(fpc, 0L, SEEK_END);
+	lSize = ftell(fpc);
+	rewind(fpc);
+
+	buffer = calloc(1, lSize+1);
+	if(fread(buffer, lSize, 1, fpc) != 1){
+	    free(buffer);
+	    exit(1);
+	}
+	fclose(fpc);
+	//mvaddstr(20,20, buffer);
+
+	/*DO NOT DELETE
+	erase();
+	int place = 30;
+	char * choice = strtok(buffer, "\n");
+	while(choice != NULL){
+	    mvaddstr(place++, 15, choice);
+	    choice = strtok(NULL, "\n");
+	}*/
+
+
+
+	//TempList * tempList = malloc(sizeof(TempList));
+	//TempList * list = tempList;
+	/*while(fscanf(fpc, "%s", buffer) != EOF){
 		strcpy(tempList->string, buffer);
 		tempList->next = malloc(sizeof(TempList));
 		tempList = tempList->next;
-	}
-	tempList = list;
+	}*/
+	//tempList = list;
 	int count = 0;
-	while(tempList->next != NULL){
-            char * curClass[30];
+	/*while(tempList->next != NULL){
+            char * curClass[1];
 	    if(strlen(tempList->string) != 5){
 		strcpy(curClass, tempList->string);
 	    }
@@ -96,30 +119,28 @@ int main(void) {
 		//tempList = tempList->next;
 	    }
 	    tempList = tempList->next;
-	}
-	**teacher->classes = realloc(teacher->classes, sizeof(teacher->classes) * count);
+	}*/
+	//*teacher->classes = realloc(teacher->classes, sizeof(teacher->classes) * count);
 	int position = 0;
-	while(list->next != NULL){
-	    char * curClass[30];
+	/*while(list->next != NULL){
+	    char * curClass[1];
 	    if(strlen(tempList->string) != 5){
 		strcpy(curClass, tempList->string);
+		mvaddstr(5,5,tempList->string);
 	    }
 	    else{
 		if(strcmp(id_string, tempList->string) == 0){
-		    strcpy(teacher->classes[position][0], curClass);
+		    *teacher->classes[position] = *curClass;
 		    position++;
 		}
 	    }
+	    list = list->next;
 	}
-	if(strcmp(teacher->classes[2][0], "c") == 0){
-	    mvaddstr(5, 5, "This actually works");
-	}
+	mvaddstr(2,2,teacher->classes[1]);*/
     }
     else{
 	Student * student = malloc(sizeof(Student));
-	TempList * tempList = malloc(sizeof(TempList));
-	TempList * list = tempList;
-	char buffer[50];
+	char buffer[50];/*
 	while(fscanf(fpc, "%s", buffer) != EOF){
 	    if(strlen(buffer) != 5){
 		tempList->next = malloc(sizeof(TempList));
@@ -127,15 +148,15 @@ int main(void) {
 	        tempList = tempList->next;
 	    }
 	}
-	fclose(fpc);
+	fclose(fpc);*/
 	
     }
 
-    mvaddstr(15, 33, id_check);
+    mvaddstr(15, 33, id_string);
     mvaddstr(16, 33, firstName);
     mvaddstr(17, 33, lastName);
     refresh();
-    sleep(1e99);
+    sleep(60);
 
 
     /*  Clean up after ourselves  */
